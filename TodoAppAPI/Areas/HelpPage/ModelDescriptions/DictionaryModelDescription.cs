7ed978b9718b769f22530ee5567fae9d2a8bfc16
reemak <reemakuvadia@gmail.com>
2015-11-23 07:12:58 -0500

    Initial Commit

---
+++ TodoAppAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TodoAppAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TodoAppAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TodoAppAPI.WebApiApplication" Language="C#" %>
