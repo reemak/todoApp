55c73f4f409dc04b461d239c6daf64135c233a09
reemak <reemakuvadia@gmail.com>
2015-11-23 14:19:26 -0500

    Set up the Bundle config and loaded all the required js and css files on the main page

--- TodoApp/App_Start/BundleConfig.cs
+++ TodoApp/App_Start/BundleConfig.cs
@@ -9,20 +9,29 @@ namespace TodoApp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/bower_components/jquery/dist/jquery.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bower_components").Include(
+                    "~/bower_components/bootstrap/dist/js/bootstrap.min.js"
+                    , "~/bower_components/angular/angular.min.js"
+                    , "~/bower_components/angular-bootstrap/ui-bootstrap.min.js"
+                    , "~/bower_components/angular-ui-router/release/angular-ui-router.min.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/bower_components/bootstrap/dist/css/bootstrap.min.css"
+                      , "~/Content/site.css"
+                     
+                      ));
+
+            bundles.Add(new ScriptBundle("~/app").Include(
+                "~/app/app.js"
+                ));
         }
     }
 }
