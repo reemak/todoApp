01f34f07a70a8493c2f61199fd2aa1baac49faf0
reemak <reemakuvadia@gmail.com>
2015-11-29 07:46:14 -0500

    Added edmx and entity framework to read and write to the database

---
+++ TodoApp/Controllers/ListNamesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TodoApp.Database;
+
+namespace TodoApp.Controllers
+{
+    public class ListNamesController : ApiController
+    {
+        // GET: api/ListName
+        public IEnumerable<string> Get()
+        {
+            List<string> lists = new List<string>();
+            return lists;
+        }
+
+        // GET: api/ListName/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ListName
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ListName/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ListName/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
